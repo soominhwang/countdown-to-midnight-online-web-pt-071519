af1788c97108de3033a2876667f1f29eb4fb9092
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-22 05:11:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,11 @@ def countdown(number)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep
+  while number>0
+    puts "#{number} SECOND(S)!"
+    number -=1
+    sleep 1
+  end
+  return "HAPPY NEW YEAR!"
